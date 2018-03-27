433e67b9f645c2cdebfde4d07e238454f636c5d1
maylisw <Maylisw31@gmail.com>
2018-03-27 13:21:47 -0700

    deleted settings

--- src/BackendlessSettings.java
+++
@@ -1,4 +0,0 @@
-public class BackendlessSettings {
-        public static final String  APP_ID = "DBA01805-8294-67C0-FF77-3A5D083F5800";
-        public static final String  API_KEY = "F2C18E17-5472-4B71-FF38-56179F719600";
-}
